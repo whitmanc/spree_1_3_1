5ca3314b79545f23706d47de4ed2c8ebdf5eb570
Chris Whitman <chris@startupminds.com>
2013-01-13 01:38:11 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JewelryStore::Application.config.secret_token = '57d7dcba730f7167744872a7e001f515b671aa7943b0fc9392005dab3428f3e3a22687391872ef2b8b5b4d39245a0945167f5c9b9a635a29f8232917a2e880be'
