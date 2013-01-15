5b432a28ef55cf3bd34444f313674d4c8751e330
Chris Whitman <chris@startupminds.com>
2013-01-14 20:24:15 -0800

    Revert Gemfile changes; try to fix deploy via disable init on precompile

--- config/application.rb
+++ config/application.rb
@@ -71,5 +71,8 @@ module JewelryStore
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # To fix SASS compiling in production
+    config.assets.initialize_on_precompile = false
   end
 end
