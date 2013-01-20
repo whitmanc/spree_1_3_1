4ba6eff2f3773ad7673f08a488e040434ad5837f
Chris Whitman <chris@startupminds.com>
2013-01-19 20:03:32 -0800

    remove deface config from application.rb

--- config/application.rb
+++ config/application.rb
@@ -71,7 +71,5 @@ module JewelryStore
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-
-    config.deface.enabled = false
   end
 end
